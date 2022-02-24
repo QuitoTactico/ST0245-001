affbb3ceee2d9e3218ac90c973dc8e2bb6a92234
QuitoTactico <99926526+QuitoTactico@users.noreply.github.com>
2022-02-24 18:47:04 -0500

    Add files via upload

---
+++ codigo/abrirArchivoCalles.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv("calles_de_medellin_con_acoso.csv", sep=';')
+print(df)
---
+++ codigo/abrirArchivoPoligono.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv("poligono_de_medellin.csv", sep=';')
+print(df)
